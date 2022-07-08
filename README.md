3ab73da265ce12f8139e92db89dd0c280e753931
Yenifer-lizcano <milenalizcano00@gmail.com>
2022-07-08 16:37:06 -0500

    des-1: documentacion akela

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # MiProyecto
 Primer repositorio de Github
 
-
 Más líneasss
+
+# Documentación 1 (Des-1)
--- main.c
+++ main.c
@@ -1,4 +1,30 @@
 int main (void)
 {
     printf("Hola Mundo");
+    // git clone <repo>
+    // git fetch (una vez clonado el repositorio, descarga la informacion)
+    // git pull origin main (jala el commit al local)
+    // git commit -am (guardar de manera local)
+    // git push origin main (empujar los cambios al servidor remoto)
+
+    // git branch (nombre de la nueva linea) crear linea
+    // git checkout (nombre de la linea)
+    // git push origin (nombre del branch) (empujar los cambios Branch)
+    // git checkout (nombre de donde se quiere posicionarse)
+
+    //Otra frma de crear un BRANCH
+    // git checkout -B (+nombre del la nueva linea)(crea el branch y se posiciona en él)
+
+    //crear una nueva linea o branches o una rama, con la misma historia (commit) del main principal
+    //Ambos trabajan en el mismo main y con la misma historia, pero en diferentes lienas 
+    // para eso se utiliza "git branch y el nombre de la nueva linea que se despliega del main"
+    // despues de creada la linea, para moverse se utiliza un nuevo comando
+
+    //JUNTAR BRANCH CON EL MAIN
+    // Pull Request (en Github)
+    // despues que aceptan el pull request
+    //Merge Pull Request - se confirma 
+
+
 } 
+
